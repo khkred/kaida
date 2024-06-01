8e7cff368d3c0a9c8161b4719677ac65330c0cc6
Harish Kunchala <32921476+khkred@users.noreply.github.com>
2024-06-01 16:03:00 -0700

    Tutorial 2: Implementing Sign Up and Sign In UI with Flutter Hooks

--- lib/src/features/auth/presentation/pages/signin_page.dart
+++ lib/src/features/auth/presentation/pages/signin_page.dart
@@ -1,13 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SignInPage extends StatelessWidget {
+import '../../../../constants/routes.dart';
+
+class SignInPage extends HookConsumerWidget {
   const SignInPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign In Page', style: TextStyle(fontSize: 26),),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = useMemoized(() => GlobalKey<FormState>());
+
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+
+    final errorMessage = useState<String?>(null);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign In'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              //Email TextFormField
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+
+                  // Email Format Validator using Regex
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+
+                  return null;
+                },
+              ),
+
+              //Password TextFormField
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  }
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(
+                height: 20,
+              ),
+              //Show the error here
+              if (errorMessage.value != null)
+                Text(
+                  errorMessage.value!,
+                  style: const TextStyle(color: Colors.red),
+                ),
+
+              //Sign In Button
+              ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState?.validate() == true) {
+                      try {
+                        //This will be replaced by the actual sign in logic in future tutorials
+                        // final authNotifier = ref.read(authNotifierProvider.notifier);
+                        // await authNotifier.signIn(emailController.text, passwordController.text);
+
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text('Form validated successfully')));
+                      } catch (e) {
+                        errorMessage.value = e.toString();
+                      }
+                    }
+                  },
+                  child: const Text('Sign In')),
+
+              //Go to SignUp
+              TextButton(
+                  onPressed: () {
+                    context.go(Routes.signUp);
+                  },
+                  child: const Text('Don\'t have an account? Sign Up')),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/src/features/auth/presentation/pages/signup_page.dart
+++ lib/src/features/auth/presentation/pages/signup_page.dart
@@ -1,13 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SignUpPage extends StatelessWidget {
+import '../../../../constants/routes.dart';
+
+class SignUpPage extends HookConsumerWidget {
   const SignUpPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Sign Up Page', style: TextStyle(fontSize: 26),),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final formKey = useMemoized(() => GlobalKey<FormState>());
+
+    final nameController = useTextEditingController();
+    final emailController = useTextEditingController();
+    final phoneController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final confirmPasswordController = useTextEditingController();
+
+    final errorMessage = useState<String?>(null);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sign Up'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: formKey,
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+              ),
+              //Email TextFormField
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+
+                  // Email Format Validator using Regex
+                  if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+
+                  return null;
+                },
+              ),
+
+              TextFormField(
+                controller: phoneController,
+                decoration: const InputDecoration(labelText: 'Phone Number'),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your phone number';
+                  }
+
+                  // Phone Number Validator using Regex
+                  if (!RegExp(r'^\d{10}$').hasMatch(value)) {
+                    return 'Please enter a valid phone number';
+                  }
+
+                  return null;
+                },
+              ),
+
+              //Password TextFormField
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  }
+
+                  return null;
+                },
+              ),
+
+              TextFormField(
+                controller: confirmPasswordController,
+                decoration:
+                    const InputDecoration(labelText: 'Confirm Password'),
+                obscureText: true,
+                validator: (value) {
+                  // This will cover all the scenarios
+                  if (value != passwordController.text) {
+                    return 'Passwords do not match';
+                  }
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 20,),
+              //Show the error here
+              if (errorMessage.value != null)
+                Text(errorMessage.value!,
+                style: const TextStyle(color: Colors.red),),
+
+              ElevatedButton(onPressed: () async{
+                if(formKey.currentState?.validate() == true) {
+                  try {
+                    // This will be replaced by actual sign-up logic in future tutorials
+                    // final authNotifier = ref.read(authNotifierProvider.notifier);
+                    // await authNotifier.signUp(emailController.text, passwordController.text, nameController.text, phoneController.text)
+
+                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Form validated successfully')));
+                  }
+                  catch (e) {
+                    errorMessage.value = e.toString();
+                  }
+                }
+              }, child: const Text('Sign Up')),
+
+
+              TextButton(onPressed: (){
+                context.go(Routes.signIn);
+              }, child: const Text('Already have an account? Sign In'))
+            ],
+          ),
+        ),
       ),
     );
   }
